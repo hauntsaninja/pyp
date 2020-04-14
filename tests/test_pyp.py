c282de00b3363834c99985c3fc0b664f9303e2c5
hauntsaninja <>
2020-04-13 23:55:58 -0700

    test_pyp: add a test, use kwargs for readability

--- tests/test_pyp.py
+++ tests/test_pyp.py
@@ -1,4 +1,5 @@
 import ast
+import shlex
 import subprocess
 import sys
 from typing import Optional
@@ -24,59 +25,77 @@ def compare_command(
 
 
 def test_examples():
-    """Based on examples in the README."""
+    """Test the examples in the README."""
 
-    INPUT = "python\nsnake\nss\nmagpy\npiethon\nreadme.md\n"
+    example = "python\nsnake\nss\nmagpy\npiethon\nreadme.md\n"
 
-    compare_command("cut -c1-4", "pyp 'x[:4]'", input=INPUT)
-
-    compare_command("wc -c | tr -d ' '", "pyp 'len(stdin.read())'", input=INPUT)
-
+    compare_command(example_cmd="cut -c1-4", pyp_cmd="pyp 'x[:4]'", input=example)
     compare_command(
-        "awk '{s+=$1} END {print s}' ", "pyp 'sum(map(int, lines))'", input="1\n2\n3\n4\n5\n"
+        example_cmd="wc -c | tr -d ' '", pyp_cmd="pyp 'len(stdin.read())'", input=example
     )
-
-    compare_command("sh", "pyp 'subprocess.run(lines[0], shell=True); pass'", input="echo echo")
-
     compare_command(
-        'jq .[1]["lol"]',
-        """pyp 'json.load(stdin)[1]["lol"]'""",
+        example_cmd="awk '{s+=$1} END {print s}' ",
+        pyp_cmd="pyp 'sum(map(int, lines))'",
+        input="1\n2\n3\n4\n5\n",
+    )
+    compare_command(
+        example_cmd="sh",
+        pyp_cmd="pyp 'subprocess.run(lines[0], shell=True); pass'",
+        input="echo echo",
+    )
+    compare_command(
+        example_cmd='jq .[1]["lol"]',
+        pyp_cmd="""pyp 'json.load(stdin)[1]["lol"]'""",
         input='[0, {"lol": "hmm"}, 0]',
         allow_example_fail=True,
     )
-
     compare_command(
-        "grep -E '(py|md)'", """pyp 'x if re.search("(py|md)", x) else None'""", input=INPUT
+        example_cmd="grep -E '(py|md)'",
+        pyp_cmd="""pyp 'x if re.search("(py|md)", x) else None'""",
+        input=example,
     )
-
-    compare_command("echo 'sqrt(9.0)' | bc", "pyp 'sqrt(9)'")
-
-    compare_command("x=README.md; echo ${x##*.}", '''x=README.md; pyp "Path('$x').suffix[1:]"''')
-
-    compare_command("echo '  1 a\n  2 b'", """pyp 'f"{idx+1: >3} {x}"'""", input="a\nb")
-
-    compare_command("grep py", """pyp 'x if "py" in x else None'""", input=INPUT)
-
-    compare_command("tail -n 3", "pyp 'lines[-3:]'", input=INPUT)
-
-    compare_command("sort", "pyp 'sorted(lines)'", input=INPUT)
+    compare_command(example_cmd="echo 'sqrt(9.0)' | bc", pyp_cmd="pyp 'sqrt(9)'")
     compare_command(
-        "echo 'Sorting 2 lines\n1\n2'",
-        """pyp 'print(f"Sorting {len(lines)} lines"); pypprint(sorted(lines))'""",
+        example_cmd="x=README.md; echo ${x##*.}",
+        pyp_cmd='''x=README.md; pyp "Path('$x').suffix[1:]"''',
+    )
+    compare_command(
+        example_cmd="echo '  1 a\n  2 b'", pyp_cmd="""pyp 'f"{idx+1: >3} {x}"'""", input="a\nb"
+    )
+    compare_command(
+        example_cmd="grep py", pyp_cmd="""pyp 'x if "py" in x else None'""", input=example
+    )
+    compare_command(example_cmd="tail -n 3", pyp_cmd="pyp 'lines[-3:]'", input=example)
+    compare_command(example_cmd="sort", pyp_cmd="pyp 'sorted(lines)'", input=example)
+    compare_command(
+        example_cmd="echo 'Sorting 2 lines\n1\n2'",
+        pyp_cmd="""pyp 'print(f"Sorting {len(lines)} lines"); pypprint(sorted(lines))'""",
         input="2\n1\n",
     )
-    compare_command("sort | uniq", "pyp 'sorted(set(lines))'", input="2\n1\n1\n1\n3")
+    compare_command(
+        example_cmd="sort | uniq", pyp_cmd="pyp 'sorted(set(lines))'", input="2\n1\n1\n1\n3"
+    )
+    compare_command(
+        example_cmd='''echo "a: ['1', '3']\nb: ['2']"''',
+        pyp_cmd="pyp -b 'd = defaultdict(list)' 'k, v = x.split(); d[k].append(v)' -a 'd'",
+        input="a 1\nb 2\na 3",
+    )
 
 
 def test_explain():
-    command = (
-        "pyp --explain "
-        "-b 'd = defaultdict(list)' "
-        "'user, pid, *_ = x.split()' "
-        "'d[user].append(pid)' "
-        """-a 'del d["root"]' -a 'd'"""
-    )
-    explain_output = run_cmd(command).decode("utf-8")
+    command = [
+        "pyp",
+        "--explain",
+        "-b",
+        "d = defaultdict(list)",
+        "user, pid, *_ = x.split()",
+        "d[user].append(pid)",
+        "-a",
+        'del d["root"]',
+        "-a",
+        "d",
+    ]
+    explain_output = run_cmd(" ".join(map(shlex.quote, command))).decode("utf-8")
     script = r"""
 from collections import defaultdict
 from pyp import pypprint
