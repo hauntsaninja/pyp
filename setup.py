319c6a251c992e1c11e44c0ba2b6e8617150e080
hauntsaninja <>
2020-04-13 17:58:16 -0700

    pyp: add a setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="pyp",
+    version="0.1",
+    author="Shantanu Jain",
+    author_email="hauntsaninja@gmail.com",
+    description="Easily run Python at the shell!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hauntsaninja/pyp",
+    classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development",
+        "Topic :: Utilities",
+    ],
+    py_modules=["pyp"],
+    entry_points={"console_scripts": ["pyp=pyp:main"]},
+    install_requires=["astunparse; python_version<'3.9'"],
+    python_requires=">=3.6",
+)
