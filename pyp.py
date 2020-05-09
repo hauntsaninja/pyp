6d5848d7eb3bd673e53453b240e9a13ae1d7294d
Nick ODell <nickodell@gmail.com>
2020-05-09 17:55:47 -0600

    Traverse if of list comprehension after assignment of loop var

--- pyp.py
+++ pyp.py
@@ -50,7 +50,7 @@ def find_names(tree: ast.AST) -> Tuple[Set[str], Set[str]]:
         def generic_visit(self, node: ast.AST) -> None:
             # Adapted from ast.NodeVisitor.generic_visit, but re-orders traversal a little
             def order(f_v: Tuple[str, Any]) -> int:
-                return {"generators": -1, "target": 1, "targets": 1, "body": 2}.get(f_v[0], 0)
+                return {"generators": -1, "target": 1, "targets": 1, "body": 2, "ifs": 2}.get(f_v[0], 0)
 
             for _field, value in sorted(ast.iter_fields(node), key=order):
                 if isinstance(value, list):
--- tests/test_find_names.py
+++ tests/test_find_names.py
@@ -34,6 +34,7 @@ def test_comprehensions():
     assert ({"x"}, {"x"}) == find_names(ast.parse("(x for x in x)"))
     assert ({"x", "xx"}, {"xxx"}) == find_names(ast.parse("(x for xx in xxx for x in xx)"))
     assert ({"x", "xx"}, {"xx", "xxx"}) == find_names(ast.parse("(x for x in xx for xx in xxx)"))
+    assert ({"x"}, {"xx"}) == find_names(ast.parse("(x for x in xx if x == 'foo')"))
 
 
 def test_args():
