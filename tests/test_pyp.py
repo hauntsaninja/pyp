a598ac506bb6537eb521b79a24507cce8cd0d441
Bobronium <appkiller16@gmail.com>
2022-07-30 04:25:14 +0400

    Fix imports and style

--- tests/test_pyp.py
+++ tests/test_pyp.py
@@ -40,8 +40,12 @@ def run_pyp(cmd: Union[str, List[str]], input: Optional[str] = None) -> str:
     if cmd[0] == "pyp":
         del cmd[0]
 
+    if input and isinstance(input, str):
+        input = input.encode()
+
     output = io.StringIO()
-    with contextlib.redirect_stdout(output), BytesIO(input.encode()) as bio, TextIOWrapper(bio, encoding='utf-8') as stdin, patch("sys.stdin", stdin):
+    stdin = io.TextIOWrapper(io.BytesIO(input), encoding='utf-8')
+    with contextlib.redirect_stdout(output), patch("sys.stdin", stdin):
         pyp.run_pyp(pyp.parse_options(cmd))
     return output.getvalue()
 
